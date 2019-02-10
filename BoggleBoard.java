757c6ae73b81424b62984be7c1131244ffdd39cf
Silvio Manuel <dr.silvio.manuel@gmail.com>
2019-02-11 00:03:41 +0200

    An initial functionality... underway

--- BoggleBoard.java
+++ BoggleBoard.java
@@ -1,45 +1,83 @@
+import edu.princeton.cs.algs4.In;
+
 public class BoggleBoard {
+
+    private final int rows;
+    private final int cols;
+    private final char[][] board;
     // Initializes a random 4-by-4 Boggle board.
     // (by rolling the Hasbro dice)
     public BoggleBoard() {
-        throw new RuntimeException("Not implemented");
+        this(4, 4);
     }
 
     // Initializes a random m-by-n Boggle board.
     // (using the frequency of letters in the English language)
     public BoggleBoard(int m, int n) {
-        throw new RuntimeException("Not implemented");
+        rows = m;
+        cols = n;
+        board = new char[rows][cols];
     }
 
     // Initializes a Boggle board from the specified filename.
     public BoggleBoard(String filename) {
-        throw new RuntimeException("Not implemented");
+        throwExceptionIfNull(filename);
+
+        In input = new In(filename);
+        String line  = input.readLine();
+        throwExceptionIfNull(line);
+
+        String[] dims = line.trim().split(" ");
+        rows = Integer.parseInt(dims[0]);
+        cols = Integer.parseInt(dims[1]);
+        board = new char[rows][cols];
+        int i = 0;
+        while (input.hasNextLine()) {
+            line = input.readLine();
+            int j = 0;
+            for (int n = 0; n < line.length(); n++) {
+                char c = line.charAt(n);
+                if (c == ' ') continue;
+
+                board[i][j++] = c;
+
+                if (c == 'Q') n++;
+            }
+            i++;
+        }
     }
 
     // Initializes a Boggle board from the 2d char array.
     // (with 'Q' representing the two-letter sequence "Qu")
     public BoggleBoard(char[][] a) {
-        throw new RuntimeException("Not implemented");
+        board = a;
+        rows = a.length;
+        cols = a[0].length;
     }
 
     // Returns the number of rows.
     public int rows() {
-        throw new RuntimeException("Not implemented");
+        return rows;
     }
 
     // Returns the number of columns.
     public int cols() {
-        throw new RuntimeException("Not implemented");
+        return cols;
     }
 
-    // Returns the letter in row i and column j.
+    // Returns the letter in rows i and column j.
     // (with 'Q' representing the two-letter sequence "Qu")
     public char getLetter(int i, int j) {
-        throw new RuntimeException("Not implemented");
+        return board[i][j];
     }
 
     // Returns a string representation of the board.
     public String toString() {
         throw new RuntimeException("Not implemented");
+    }
+
+    private static void throwExceptionIfNull(Object... args) {
+        for (Object arg : args)
+            if (arg == null) throw new IllegalArgumentException();
     }
 }
--- BoggleSolver.java
+++ BoggleSolver.java
@@ -1,18 +1,84 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.TST;
+
+import java.util.Collections;
+import java.util.HashSet;
+
 public class BoggleSolver {
+
+    private final TST<Boolean> dict = new TST<>();
+
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-        throw new RuntimeException("Not implemented");
+        for (String word : dictionary) {
+            dict.put(word, Boolean.TRUE);
+        }
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        throw new RuntimeException("Not implemented");
+        int rows = board.rows();
+        int cols = board.cols();
+        TST<Boolean> boardDict = new TST<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                char c = board.getLetter(i, j);
+                Sequence w = new Sequence();
+                w.add(i, j, c);
+                w.move();
+            }
+        }
+
+
+        return Collections.EMPTY_LIST;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
         throw new RuntimeException("Not implemented");
+    }
+
+    class Sequence {
+        StringBuilder chars = new StringBuilder();
+        int row;
+        int col;
+        int rowCnt;
+        int colCnt;
+        byte pointer;
+        HashSet<Integer> rows;
+        HashSet<Integer> cols;
+        int pos[][] = {
+                {-1, -1}, {-1, 0}, {-1, 1},
+                {0, -1}, {0, 1},
+                {1, -1}, {1, 0}, {1, 1}
+        };
+
+        void add(int r, int c, char ch) {
+            row = r;
+            col = c;
+            rows.add(r);
+            cols.add(c);
+            chars.append(ch);
+            pointer = 0;
+        }
+
+        void move() {
+            int[] nextPos = calcRowCol();
+        }
+
+        int[] calcRowCol() {
+            int[] shift = null;
+            do {
+                shift = pos[pointer++];
+                if (shift[0] + row >= 0 && shift[1] + col >= 0 && shift[0] + row < rowCnt && shift[1] + col < colCnt) {
+                    return shift;
+                }
+            } while (pointer < pos.length);
+            return null;
+        }
     }
 }
